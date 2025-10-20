22e5ce1ca348e63cd5aed5b184c3bbb0356646ac
Elida <blubber3210@gmail.com>
2025-10-20 11:25:35 +0200

    main view function

---
+++ view/view.js
@@ -0,0 +1,19 @@
+const container = document.getElementById("container");
+
+updateView();
+function updateView(){
+    if (model.app.currentPage == "loginPage"){
+        showLoginPage()
+    } else if (model.app.currentPage == "mainFeed"){
+        showMainFeedPage()
+    } else if (model.app.currentPage == "addPlaces"){
+        showAddPlacesPage()
+    } else if (model.app.currentPage == "placePage"){
+        showPlacePage()
+    } else if (model.app.currentPage == "registerUser"){
+        showRegisterUserPage()
+    } else {
+        container.innerHTML = "something went wrong"
+    }
+}
+
