c51736d3e21d9c8ccdf8d700153a4db3e9fb68aa
Kamillaaks <kammi.ar@hotmail.com>
2025-10-28 13:14:18 +0100

    functions added

--- controller/registerUserController.js
+++ controller/registerUserController.js
@@ -1,7 +1,31 @@
 function addNewUser() {
+    let newUser = model.viewState.registerUser;
 
+    model.data.users.push(
+        {
+            id: 10,
+            userName: newUser.username,
+            email: newUser.email,
+            password: newUser.password,
+            placesAdded: [],
+        },
+    )
+    console.log(model.data.users)
+    emptyRegisterUserInput();
+    changePage("loginPage");
 }
 
-function registerNewUser() {
-
+function emptyRegisterUserInput() {
+    model.viewState.registerUser.username = '';
+    model.viewState.registerUser.email = '';
+    model.viewState.registerUser.password = '';
+    model.viewState.registerUser.repeatPassword = '';
 }
+
+
+/* 
+- Få den nye brukeren i data
+- Lage en funksjon som generer en tilfeldig id
+- Sjekke om passord samsvarer
+- Om bruker eksisterer fra før
+*/
