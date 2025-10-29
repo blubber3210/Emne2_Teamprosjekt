df004c716a22c1c4c18ea41a72ec6fff3cf18243
Elida <blubber3210@gmail.com>
2025-10-29 14:11:58 +0100

    addPlace-files changes

---
+++ controller/addPlaceController.js
@@ -0,0 +1,20 @@
+function submitPlaceInputs(){
+
+}
+
+function categoryInput(){
+
+}
+
+function ratingCoffeeInput(rating){
+
+}
+
+function ratingFoodInput(rating){
+
+}
+
+function submitPlaceInputs(){
+
+}
+
--- view/AddPlacesPage.js
+++ view/AddPlacesPage.js
@@ -1,28 +1,48 @@
 function showAddPlacesPage(){
     let html = /*HTML*/ `
     <div class="addPlacePage">
-    <h2>legg til sted</h2>
-        <div class="inputField">
-            Navn på sted:<br>
+    <h2 class="addPlaceHeader">Legg til sted</h2>
+        <div class="inputFields">
+            Navn:<br>
             <input type="text" oninput=${model.viewState.addPlace.title = this.value}><br> 
             By:<br>
             <input type="text" oninput=${model.viewState.addPlace.city = this.value}><br>
             Adresse:<br>
             <input type="text" oninput=${model.viewState.addPlace.address = this.value}><br>
         </div>
-            <label for="myDropdown">Kategorier:</label>
-            <select id="myDropdown" name="myDropdown">
-            <option value="cafeSelect">cafe</option>
-            <option value="restaurantSelect">restaurant</option>
-            <option value="bakerySelect">bakery</option>
-            </select> <br>
-            <button>upload image</button> <br>
-            <button>rate coffee</button> <br>
-            <button>rate food </button><br>
+        <div>
+            <div>
+                Kategori: <br>
+                <input type="checkbox" onchange="categoryInput('cafe')"> cafe
+                <input type="checkbox" onchange="categoryInput('restaurant')"> restaurant
+                <input type="checkbox" onchange="categoryInput('bakery')"> bakeri
+            </div>
+            <label for="imageUpload">Last opp bilde:</label>
+            <input type="file" id="imageUpload" name="imageUpload" accept="image/*">
+            
+            <div class="star-rating">
+                Kaffe:
+                <span class="star" onclick="ratingCoffeeInput(1)">★</span>
+                <span class="star" onclick="ratingCoffeeInput(2)">★</span>
+                <span class="star" onclick="ratingCoffeeInput(3)">★</span>
+                <span class="star" onclick="ratingCoffeeInput(4)">★</span>
+                <span class="star" onclick="ratingCoffeeInput(5)">★</span>
+            </div>
+            <div class="star-rating">
+                Mat:
+                <span class="star" onclick="ratingFoodInput(1)">★</span>
+                <span class="star" onclick="ratingFoodInput(2)">★</span>
+                <span class="star" onclick="ratingFoodInput(3)">★</span>
+                <span class="star" onclick="ratingFoodInput(4)">★</span>
+                <span class="star" onclick="ratingFoodInput(5)">★</span>
+            </div>
+
+
             Beskrivelse: <br>
             <input type="text" oninput=${model.viewState.addPlace.description = this.value}> <br>
-            <button>legg ut</button>
-
+            <input type="checkbox" onchange="submitAnonymouslySelected()">Legg ut som spooky <br>
+            <button onclick="submitPlaceInputs()">Legg ut</button>
+        </div>
     </div>
     `;
     return html; 
