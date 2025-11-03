24adf001d1edcccc4436d1c8d2132a5a98eb0933
TomDworek <tomgaming@hotmail.no>
2025-11-03 09:44:12 +0100

    Updated style an folder structure
    Oppdaterte style og koden til loginPage også lastet opp favicon og logo.

--- controller/loginController.js
+++ controller/loginController.js
@@ -4,22 +4,20 @@ function login() {
     const usernameInput = login.userName.trim().toLowerCase(); // Gjør brukernavnet små bokstaver og fjerner mellomrom for å unngå skrivefeil
     const passwordInput = login.password;  // Henter passord fra input
     const anonymous = login.anonymous; // Sjekker om "anonym" checkbox er valgt
-
-    const user = model.data.users.find(
+    const user = model.data.users.find(// Søker etter en bruker i databasen (model.data.users) som matcher brukernavn og passord
         u => u.userName.toLowerCase() === usernameInput && u.password === passwordInput
-    );  // Søker etter en bruker i databasen (model.data.users) som matcher brukernavn og passord
-    
-    const messageEl = document.getElementById('message'); // Henter HTML-elementet der feilmeldinger skal vises
-    
+    );
+    const LEmessageEl = document.getElementById('loginErrorMessage'); // Henter HTML-elementet der feilmeldinger skal vises
     if (user) {// Hvis brukeren finnes (riktig brukernavn og passord)
         model.app.currentUser = anonymous ? 'Spooky bruker' : user.userName; // Hvis anonym er valgt, logg inn som "Spooky bruker", ellers med ekte brukernavn
         model.app.currentPage = 'mainFeed'; // Gå til mainFeed etter innlogging
         updateView();
     } else {// Hvis brukeren ikke finnes, vis feilmelding
-        messageEl.textContent = 'Feil brukernavn eller passord';
-        messageEl.style.color = 'red';
+        LEmessageEl.textContent = 'Feil brukernavn eller passord';
+        LEmessageEl.style.color = 'red';
     }
 }
+
 // Funksjon for å logge ut brukeren
 function logout() {
     model.app.currentUser = null; // Fjerner nåværende bruker
@@ -28,6 +26,7 @@ function logout() {
     model.viewState.login.anonymous = false; // Setter anonym-checkbox tilbake til false
     updateView();
 }
+
 // Funksjon for å logge inn som anonym bruker direkte (uten vanlig innlogging)
 function loginAsAnonymous(){
     model.app.currentUser = 'Spooky'; // Setter brukernavn til "Spooky"
