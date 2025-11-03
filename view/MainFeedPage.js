5e24e637fd4a7e36d1e98390b9ddc364e9383ca0
c89dev <snarvei@live.com>
2025-11-03 14:53:21 +0100

    feed adjustments

--- view/MainFeedPage.js
+++ view/MainFeedPage.js
@@ -1,42 +1,46 @@
 function showMainFeedPage() {
-    let html = /*HTML*/ `
-    <button onclick="changePage('addPlaces')">Legg til plass</button>
-    <input 
-        placeholder="Søk by/adresse" 
+  let html = /*HTML*/ `
+  <div class="feedControls"
+    <button onclick="changePage('addPlaces')">➕ Legg til sted</button>
+    <br>
+    <input
+        placeholder="Søk by/adresse"
         oninput="model.viewState.mainFeed.search = this.value"
         type="text">
-    <button onclick="updateView()">search</button>
+    <button onclick="updateView()">🔍</button>
     <div id="feedDiv">
     ${drawFeed()}
     </div>
     `;
 
-    return html;
+  return html;
 }
 
 function drawFeed() {
-    let places = filterPlaces();
-    let html = ``;
+  let places = filterPlaces();
+  let searchword = model.viewState.mainFeed.search;
+  let isSearching = searchword ? 'Resultater for: ' : '';
+  let html = ``;
 
-    for (let i = 0; i < places.length; i++) {
-        html += `
-        
+  for (let i = 0; i < places.length; i++) {
+    html += `
+
         <div class="feedItem">
-            <div class="filteredListImg"> 
-        <img src="${places[i].image}">
+            <div>
+        <img class="filteredListImg" src="${places[i].image}">
             </div>
             <br>
 
             <div class="filteredListInfo">
            ${places[i].title}<br>
-            <p>${"⭐".repeat(places[i].rateFood)}<br>
+            <p>${'⭐'.repeat(places[i].rateFood)}<br>
             ${places[i].city},
             ${places[i].address}<br>
             <button onclick="goToPlacePage(${places[i].id})">
             go to place page</button>
             </div>
-        
+
         </div>`;
-    }
-    return html;
+  }
+  return isSearching + searchword + html;
 }
