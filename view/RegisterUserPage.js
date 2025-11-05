bc9799ba31c120f1c1a4c42e4df2500c37a436a6
Kamillaaks <kammi.ar@hotmail.com>
2025-11-05 14:48:04 +0100

    css

--- view/RegisterUserPage.js
+++ view/RegisterUserPage.js
@@ -1,29 +1,34 @@
 function showRegisterUserPage() {
-  let html = /*HTML*/ `
-        <div>
+    let html = /*HTML*/ `
+        <div class="registerDiv">
+            <img src="media/logo.png" alt="Logo" width="120" height="120">
             <h1>Lag ny bruker</h1>
             <div>
-                <h2>Brukernavn</h2>
+                <h2 class="registerText">Brukernavn</h2>
                 <input
+                class="registerInput"
                 placeholder="Ditt brukernavn"
                 onchange="model.viewState.registerUser.username = this.value"
                 value="${model.viewState.registerUser.username ?? ''}"
                 />
                 <div id="userNameError" style="color: red;"></div>
-                <h2>Mailadresse</h2>
+                <h2 class="registerText">Mailadresse</h2>
                 <input
+                class="registerInput"
                 placeholder="Din mailadresse"
                 onchange="model.viewState.registerUser.email = this.value"
                 value="${model.viewState.registerUser.email ?? ''}"
                 />
-                <h2>Passord</h2>
+                <h2 class="registerText">Passord</h2>
                 <input
+                class="registerInput"
                 placeholder="Ditt passord"
                 onchange="model.viewState.registerUser.password = this.value"
                 value="${model.viewState.registerUser.password ?? ''}"
                 type="password"
                 />
                 <input
+                class="registerInput"
                 placeholder="Gjenta passord"
                 onchange="model.viewState.registerUser.repeatPassword = this.value"
                 value="${model.viewState.registerUser.repeatPassword ?? ''}"
@@ -31,7 +36,7 @@ function showRegisterUserPage() {
                 />
                 <div id="repeatPwError"style="color: red;"></div>
             </div>
-            <button onclick="verifyRegistration()">Lag ny bruker</button>
+            <button class="registerButton" onclick="verifyRegistration()">Lag ny bruker</button>
         </div>
     `;
 
