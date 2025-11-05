e6c0bc57e9a066c808f6f54b207fe4bd44c94b1c
Elida <blubber3210@gmail.com>
2025-11-05 14:59:25 +0100

    scrollbar stuff

--- controller/controller.js
+++ controller/controller.js
@@ -9,5 +9,6 @@ function goToPlacePage(selectedPlaceId){
     let placeID = selectedPlaceId;
     model.app.selectedPlace = model.data.placesList[placeID];
     model.app.currentPage = 'placePage';
+    document.querySelector('#container').scrollTo({ top: 0, behavior: 'smooth' });
     updateView()
 }
